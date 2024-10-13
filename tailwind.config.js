786b44cfba9190120dd7bffefedacb8f599560e1
Sajal Batra <125984550+sajalbatra@users.noreply.github.com>
2024-10-13 16:45:45 +0530

    Revert "responsivness"

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,16 +7,3 @@ module.exports = {
   plugins: [],
 }
 
-module.exports = {
-  theme: {
-    extend: {
-      screens: {
-        sm: '640px',
-        md: '768px',
-        lg: '1024px',
-        xl: '1280px',
-      },
-    },
-  },
-}
-
